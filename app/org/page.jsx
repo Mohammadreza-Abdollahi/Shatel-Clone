1e716d9ebc81a5b2d72789f2a30ec6c7cab2ee90
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2025-03-13 23:34:34 +0330

    version 0.1.6
    added catalog section

--- app/org/page.jsx
+++ app/org/page.jsx
@@ -6,6 +6,7 @@ import LandingTopSlider from "@/components/organization/sliders/LandingTopSlider
 import RightOptions from "@/components/organization/landingPageSections/RightOptions";
 import LeftOptions from "@/components/organization/landingPageSections/LeftOptions";
 import Options from "@/components/organization/landingPageSections/Options";
+import Catalog from "@/components/organization/landingPageSections/catalog";
 
 export default function Home() {
   return (
@@ -18,6 +19,9 @@ export default function Home() {
       </section>
       <section>
         <Options/>
+      </section>
+      <section>
+        <Catalog/>
       </section>
     </>
   );
---
+++ components/organization/landingPageSections/catalog.jsx
@@ -0,0 +1,10 @@
+const Catalog = () => {
+    return ( 
+        <section className="sans py-12 text-center bg-blueBackground">
+            <h2 className="sans-bold text-white text-2xl mb-5">کاتالوگ راهکارهای سازمانی شاتل</h2>
+            <button className="text-center text-white text-base bg-greenBtn hover:bg-green-500 transition-all duration-150 px-6 py-3 border border-1 border-white rounded">دریافت</button>
+        </section>
+     );
+}
+ 
+export default Catalog;
